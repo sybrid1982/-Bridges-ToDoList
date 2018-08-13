a5695e52c2af7d0d540d6760d3b0ad638deb1e6b
Sydney Bridges <sybrid@gmail.com>
2018-08-13 16:43:31 -0400

    Edit buttons now working, can edit and finish edits

--- scripts/ctrl.js
+++ scripts/ctrl.js
@@ -3,14 +3,20 @@
 function ToDoListCtrl() {
     let vm = this;
     vm.list = [];
-    vm.addToList = (newListItem) => {
+    vm.addTask = (newListItem) => {
         vm.list.push({text: newListItem});
     }
     vm.completeTask = (index) => {
         vm.list[index].classes = {completed: true};
     }
-    vm.removeFromList = (index) => {
+    vm.removeTask = (index) => {
         vm.list.splice(index, 1);
+    }
+    vm.editTask = (index) => {
+        vm.list[index].edit = true;
+    }
+    vm.completeEdit = (index) => {
+        vm.list[index].edit = false;
     }
 }
 
